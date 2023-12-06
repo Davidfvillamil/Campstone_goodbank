e11881530ecd442813b247876d5ff9e65f241532
David Villamil <davidfvillamilm@gmail.com>
2023-12-06 10:40:27 -0500

    agregar el proceso de autenticación con clerck

--- goodbank/app/(dashboard)/(routes)/dashboard/page.tsx
+++ goodbank/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 
 export default function DashboardPage() {
   return (
     <>
-      <h1>Esta pagina es la protegida (protected)</h1>
-      
+      <div>
+        <h1>Esta pagina es la protegida (protected)</h1>
+        <UserButton afterSignOutUrl='/'/>
+      </div>
     </>
   )
 }
--- goodbank/app/(landing)/page.tsx
+++ goodbank/app/(landing)/page.tsx
@@ -1,12 +1,20 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 
 export default function LandingPage() {
   return (
     <>
       <h1>Esta es la Landing Page (unprotected)</h1>
-      <Button variant='destructive' size='lg'>Create an account</Button>
-      <Button variant='destructive' size='lg'>Login</Button>
+      <div>
+        <Link href='/create-account'>
+          <Button variant='destructive' size='lg'>Create an account</Button>
+        </Link>
+
+        <Link href='sign-in'>
+          <Button variant='destructive' size='lg'>Login</Button>
+        </Link>
+      </div>
     </>
   )
 }
