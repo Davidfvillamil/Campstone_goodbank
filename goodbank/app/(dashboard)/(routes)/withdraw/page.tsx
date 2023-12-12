a6b9f547645b80d7d39cc37aad99230f8e9edbb0
David Villamil <davidfvillamilm@gmail.com>
2023-12-12 10:30:16 -0500

    agregar funcionalidad de Withdraw

--- goodbank/app/(dashboard)/(routes)/withdraw/page.tsx
+++ goodbank/app/(dashboard)/(routes)/withdraw/page.tsx
@@ -1,4 +1,61 @@
+'use client'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
+import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore';
+import firebaseConfig from "@/lib/firebaseConfig";
+import { initializeApp } from 'firebase/app';
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
 export default function Withdraw() {
+
+  const [withdrawAmount, setWithdrawAmount] = useState('');
+  const [errorWithdrawGreaterThanBalance, setErrorWithdrawGreaterThanBalance] = useState('')
+  const { user } = useUser();
+
+  const handleWithdraw = async () => {
+    try{
+      if (!user) {
+        console.error('Usuario no autenticado.');
+        return;
+      }
+      const userDocRef = doc(firestore, 'Users', user.id);
+      const userDocSnapshot = await getDoc(userDocRef);
+
+      if (userDocSnapshot.exists()) {
+        const userData = userDocSnapshot.data();
+
+        // Sumar el valor del depósito al balance actual
+        const currentBalance = userData.Balance || 0;
+        const withdrawValue = parseFloat(withdrawAmount);
+
+        if (!isNaN(withdrawValue) && withdrawValue > 0 && withdrawValue < userData.Balance) {
+          setErrorWithdrawGreaterThanBalance('')
+
+          const newBalance = currentBalance - withdrawValue;
+          // Actualizar el documento del usuario en Firestore con el nuevo balance
+          await updateDoc(userDocRef, { Balance: newBalance });
+
+          // Actualizar el estado local para reflejar el nuevo balance en la interfaz
+          setWithdrawAmount('');
+          
+        } else if(!isNaN(withdrawValue) && withdrawValue > userData.Balance){
+          setErrorWithdrawGreaterThanBalance('The amount exceds the money in your account')
+        }else{
+          setErrorWithdrawGreaterThanBalance("You can't input negative values")
+
+        }
+      } else {
+        console.error('El documento del usuario no existe.');
+      }
+
+    }catch{
+
+    }
+  }
+
     return (
       <>
         <div>
@@ -8,6 +65,15 @@ export default function Withdraw() {
             </h2>
           </div> 
         </div>
+
+       <input 
+          type="number"
+          value={withdrawAmount}
+          onChange={(e) => setWithdrawAmount(e.target.value)}
+       /> 
+
+        <Button onClick={handleWithdraw}>Deposit</Button>
+        <h2>{errorWithdrawGreaterThanBalance}</h2>
       </>
     )
   }
