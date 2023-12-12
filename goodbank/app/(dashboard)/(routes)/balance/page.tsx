b958cf8025762b727604d75983d32d7173a744ad
David Villamil <davidfvillamilm@gmail.com>
2023-12-12 11:01:58 -0500

    agregar funcionalidad de Mirar el Balance

--- goodbank/app/(dashboard)/(routes)/balance/page.tsx
+++ goodbank/app/(dashboard)/(routes)/balance/page.tsx
@@ -1,6 +1,42 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import firebaseConfig from "@/lib/firebaseConfig";
+import { initializeApp } from 'firebase/app';
 
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
 export default function Balance() {
+
+  const { user, isSignedIn, isLoaded } = useUser();
+  const [userBalance, setUserBalance] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        if (isLoaded && isSignedIn) {
+          // Verifica si el usuario ya está en Firestore
+          const userDocRef = doc(firestore, 'Users', user.id);
+          const userDocSnapshot = await getDoc(userDocRef);
+
+          if (userDocSnapshot.exists()) {
+            // Si el usuario existe, obtén el balance del documento y actualiza el estado local
+            const userData = userDocSnapshot.data();
+            setUserBalance(userData?.Balance || 0);
+          } else {
+            console.error('El documento del usuario no existe.');
+          }
+        }
+      } catch (error) {
+        console.error('Error al obtener el balance del usuario:', error);
+      }
+    };
+
+    fetchBalance(); // Llama a la función para obtener el balance del usuario
+  }, [isLoaded, isSignedIn, user]);
+  
     return (
       <>
         <div>
@@ -8,6 +44,7 @@ export default function Balance() {
             <h2 className="text-2xl md:text-4xl font-bold text-center">
               Esta es la pagina de Balance
             </h2>
+            <h1>Tu balance es de {userBalance}</h1>
           </div> 
         </div>
       </>
